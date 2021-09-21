b8a8e20350644cd51cf62a5f2ace9f8fb2c4bc0b
LuisSid55 <luiscitty4@gmail.com>
2021-09-21 18:01:35 -0300

    Começando a colocar letra

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Yung Lixo - Polaroid");
+            Console.WriteLine();
+            Console.WriteLine("Achei um porta-retrato");
+            Console.WriteLine("Cheio de foto, guardado");
+            Console.WriteLine("No fundo do meu armário");
+            Console.WriteLine("E resolvi abrir");
+            Console.WriteLine();
+            Console.WriteLine("Um Polaroid manchado");
+            Console.WriteLine("Da gente junto abraçado");
+            Console.WriteLine("Sentado na cama");
+            Console.WriteLine("Vendo o Sol subir");
+            Console.WriteLine();
+            Console.WriteLine("Teu sorriso brilhante");
+            Console.WriteLine("No topo da roda-gigante");
+            Console.WriteLine("Segurando com cuidado");
+            Console.WriteLine("A torta de chantilly");
+            Console.WriteLine();
+            Console.WriteLine("Meu olhar preocupado");
+            Console.WriteLine("Te olhando adiante");
+            Console.WriteLine("Perguntando se aquilo não ia cair");
+            Console.WriteLine();
+            Console.WriteLine("Memórias são só memórias");
+            Console.WriteLine("Que fotos contem a história");
+            Console.WriteLine("Do que eu quis construir");
+            Console.WriteLine("Onde quer que tu esteja");
+            Console.WriteLine("Não importa o que eu cante");
+            Console.WriteLine("Porque você não vai mais me ouvir");
+            Console.WriteLine();
+            Console.WriteLine("Tô correndo mas tô sempre no lugar (yo)");
+            Console.WriteLine("Um passo atrás de poder te alcançar, e eu vou"); 
+            Console.WriteLine("Tênis sujo e rasgado de fugir da dor");
+            Console.WriteLine("Mas me espera, amor, mas me espera, amor");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
