bbd1a45ff18775a4400be59ac27fb2804bf3411b
LuisSid55 <luiscitty4@gmail.com>
2021-09-21 19:14:17 -0300

    Letra completa

--- Program.cs
+++ Program.cs
@@ -6,8 +6,12 @@ namespace Musica
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine();
-            Console.WriteLine("Yung Lixo - Polaroid");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Yung Lixo ");
+            Console.ResetColor();
+            Console.WriteLine("- Polaroid");
             Console.WriteLine();
             Console.WriteLine("Achei um porta-retrato");
             Console.WriteLine("Cheio de foto, guardado");
@@ -35,17 +39,52 @@ namespace Musica
             Console.WriteLine("Não importa o que eu cante");
             Console.WriteLine("Porque você não vai mais me ouvir");
             Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("Tô correndo mas tô sempre no lugar (yo)");
             Console.WriteLine("Um passo atrás de poder te alcançar, e eu vou"); 
             Console.WriteLine("Tênis sujo e rasgado de fugir da dor");
             Console.WriteLine("Mas me espera, amor, mas me espera, amor");
             Console.WriteLine();
+            Console.WriteLine("Tô correndo mas tô sempre no lugar (yo)");
+            Console.WriteLine("Um passo atrás de poder te alcançar, e eu vou"); 
+            Console.WriteLine("Tênis sujo e rasgado de fugir da dor");
+            Console.WriteLine("Mas me espera, amor, mas me espera, amor");
+            Console.ResetColor();
             Console.WriteLine();
+            Console.WriteLine("Minha mãe me dizia");
+            Console.WriteLine("Filho segue o ditado");
+            Console.WriteLine("Quando é pra ser, será");
             Console.WriteLine();
+            Console.WriteLine("Então desesperado");
+            Console.WriteLine("Eu olho pra todo lado");
+            Console.WriteLine("Mas eu não te vejo lá");
             Console.WriteLine();
+            Console.WriteLine("Quando eu penso na vida");
+            Console.WriteLine("Acho que eu tava errado");
+            Console.WriteLine("Mas demorei muito pra me ligar");
             Console.WriteLine();
+            Console.WriteLine("Sinto o tempo parado");
+            Console.WriteLine("que já foi é passado");
+            Console.WriteLine("E não dá mais pra eu voltar");
             Console.WriteLine();
+            Console.WriteLine("Memórias são só memórias");
+            Console.WriteLine("Que fotos contem a história");
+            Console.WriteLine("Do que eu quis construir");
+            Console.WriteLine("Onde quer que tu esteja");
+            Console.WriteLine("Não importa o que eu cante");
+            Console.WriteLine("Porque você não vai mais me ouvir");
             Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Tô correndo mas tô sempre no lugar (yo)");
+            Console.WriteLine("Um passo atrás de poder te alcançar, e eu vou"); 
+            Console.WriteLine("Tênis sujo e rasgado de fugir da dor");
+            Console.WriteLine("Mas me espera, amor, mas me espera, amor");
+            Console.WriteLine();
+            Console.WriteLine("Tô correndo mas tô sempre no lugar (yo)");
+            Console.WriteLine("Um passo atrás de poder te alcançar, e eu vou"); 
+            Console.WriteLine("Tênis sujo e rasgado de fugir da dor");
+            Console.WriteLine("Mas me espera, amor, mas me espera, amor");
+            Console.ResetColor();
             Console.WriteLine();
         }
     }
